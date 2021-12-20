f63d18bfd5543040e961c8c855e27cddaf71d504
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2021-12-20 09:56:11 +0100

    removed some commented code

--- Roll-a-Ball-AR/Assets/Scripts/Simulation/PlayerControllerSimulation.cs
+++ Roll-a-Ball-AR/Assets/Scripts/Simulation/PlayerControllerSimulation.cs
@@ -11,8 +11,6 @@ namespace RollABallSimulation
         public float jumpForceScaler;
         public Joystick joystick;
         public Button jumpButton;
-        //public Text countText;
-        //public Text winText;
 
         [SerializeField]
         private GameLogicSimulation m_GameLogic;
@@ -27,7 +25,6 @@ namespace RollABallSimulation
             rb = GetComponent<Rigidbody>();
             jump = false;
             canJump = true;
-            //winText.text = "";
             jumpButton.onClick.AddListener(SetJump);
             m_GameLogic.StartGame();
         }
